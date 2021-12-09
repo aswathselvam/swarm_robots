3758746ad1bda181ffed98e8b51c65782257bd51
aswathselvam <m.aswath08@mail.com>
2021-12-09 04:10:22 -0500

    3.4 SafetyCheck class
    Added SafetyCheck Class from class diagram specifications and implemented the functions

---
+++ src/safety_check.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include "swarm_robots/safety_check.h"
+
+SafetyCheck::SafetyCheck(string ns, ros::NodeHandle* nh){
+    this->nh_ = nh;
+    this->sub_scan_ = this->nh_->subscribe("scan"+ns, 1, &Roam::ScanCallback, this);
+    this->kfov_degrees = 180;
+}
+
+void SafetyCheck::ScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+  this->laser_scan_ = *msg;
+}
+
+
+State SafetyCheck::GetSafeDirection(){
+    double repel_lateral=0;
+    double repel_drive=0;
+
+    for (int i = 0; i < this->kfov_degrees_ / 2; i++) {
+            repel_lateral += (1/scan.ranges[i])*sin(i * M_PI / 180);
+            repel_lateral += (1/scan.ranges[359-i])*sin(i * M_PI / 180);
+            
+            repel_drive += 1/scan.ranges[i]*cos(i * M_PI / 180);
+            repel_drive += 1/scan.ranges[359-i]*cos(i * M_PI / 180);
+    }
+  return State(repel_drive,repel_lateral);
+}
+
