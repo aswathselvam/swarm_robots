b82c41d4e35a2b7e259b71a0a2b875d87a91f3d1
aswathselvam <m.aswath08@mail.com>
2021-12-12 15:45:37 -0500

    3.1 Swarm Master
    Pass argc and argv messages to Agent node class to pass it in ros:init, without argc and argv, the position publisher throws Segmentation Fault

--- src/swarm_master.cpp
+++ src/swarm_master.cpp
@@ -1,13 +1,16 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include "swarm_robots/agent_node.h"
 
 using std::vector;
 using std::string;
-
+using std::cout;
 int main(int argc, char **argv) {
-  std::string ns = "";
-  AgentNode agent_node(ns);
+  cout << "argc: "<< argc << "argv: " << argv <<std::endl;
+  std::string ns = "0";
+
+  AgentNode agent_node(argc, argv, ns);
   //vector<AgentNode> agent_nodes;
   for (int i = 0; i < 2; i++) {
     //AgentNode agent_node(std::to_string(i));
