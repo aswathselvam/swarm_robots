68ff852f2ad64801ab3bcd4d20d6ecad2213e142
aswathselvam <m.aswath08@mail.com>
2021-12-12 16:05:29 -0500

    3 Add Destructor and position callback
    Added destructor to delete the NodeHandle pointer and include posiiton callback function to get the position fo jackal robot

--- src/agent_node.cpp
+++ src/agent_node.cpp
@@ -13,6 +13,8 @@
 #include <std_msgs/String.h>
 #include <string>
 #include <iostream>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Quaternion.h>
 #include "swarm_robots/agent.h"
 #include "swarm_robots/agent_node.h"
 #include "swarm_robots/path_planner.h"
@@ -21,30 +23,49 @@ using std::string;
 
 AgentNode::AgentNode(std::string ns):  Agent::Agent(ns) {
   this->agent_id = ns;
-  int argc;
-  char** argv;
-  ros::init(argc,argv,"id");
   this->nh_ = new ros::NodeHandle();
-  
   this->path_planner_ = new PathPlanner(ns, nh_);
   this->forward_kinematics_ = new ForwardKinematics();
   this->inverse_kinematics_ = new InverseKinematics(ns, nh_);
 
   //agent1/cmd_vel
-  this->vel_pub_ = this->nh_->advertise<geometry_msgs::Twist>("agent"+ns+"/cmd_vel", this->krate_, this);
+  this->vel_pub_ = this->nh_->advertise<geometry_msgs::Twist>("/jackal"+ns+"/jackal_velocity_controller/cmd_vel", this->krate_, this);
+  this->pos_sub_ = this->nh_->subscribe("/jackal"+ns+"/base_pose_ground_truth", 1, &AgentNode::PosCallback, this);
+
   this->krate_ = 20;
+  Loop();
 }
 
 void AgentNode::Loop(){
   ros::Rate loop_rate(this->krate_);
-  while(ros::ok()){
+  //while(ros::ok()){
     AgentNode::PlanPath();
     AgentNode::PerformInverseKinematics();
     AgentNode::PerformForwardKinematics();
     ros::spinOnce();
-    loop_rate.sleep();
-  }
+    //loop_rate.sleep();
+  //}
 }
+
+void AgentNode::PosCallback(const nav_msgs::Odometry::ConstPtr& msg){
+  
+  this->position_.x_ = msg->pose.pose.position.x;
+  this->position_.y_ = msg->pose.pose.position.y;
+  
+  tf::Quaternion quat;
+  quat[0] = msg->pose.pose.orientation.x;
+  quat[1] = msg->pose.pose.orientation.y;
+  quat[2] = msg->pose.pose.orientation.z;
+  quat[3] = msg->pose.pose.orientation.w;
+  
+  double roll, pitch, yaw;
+  tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+  this->position_.yaw_ = yaw;
+
+  //ROS_INFO("Seq: [%d]", msg->header.seq);
+  
+}
+
 
 
 void AgentNode::PlanPath(){
@@ -64,4 +85,10 @@ void AgentNode::PerformForwardKinematics(){
   twist_msg_.angular.y = 0;
   twist_msg_.angular.z = velocity_.yaw_;
   vel_pub_.publish(twist_msg_);
+}
+
+
+AgentNode::~AgentNode(){
+delete nh_;
+  std::cout<<"Deteletd NH";
 }
