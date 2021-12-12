2be36300e750013513b1382810e69f89e1e86071
aswathselvam <m.aswath08@mail.com>
2021-12-12 16:36:29 -0500

    3.4 Solve segmentation fault
    Fix segmentation fault caused by accessing out of bound index in laserscan range

--- src/safety_check.cpp
+++ src/safety_check.cpp
@@ -1,11 +1,13 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <string>
+#include <iostream>
 #include <cmath>
 #include "swarm_robots/safety_check.h"
 #include "swarm_robots/state.h"
 
 using std::string;
+using std::cout;
 
 SafetyCheck::SafetyCheck(string ns, ros::NodeHandle* nh): kfov_degrees_(180){
     this->nh_ = nh;
@@ -23,13 +25,17 @@ State SafetyCheck::GetSafeDirection(){
     double repel_lateral=0;
     double repel_drive=0;
 
-    for (int i = 0; i < this->kfov_degrees_ / 2; i++) {
+    //cout<<"Laser min: "<<laser_scan_.range_min;
+    //cout<<"Laser max: "<<laser_scan_.range_max;
+    //for (int i = 0; i < this->kfov_degrees_ / 2; i++) {
+    for(int i= laser_scan_.range_min; i < laser_scan_.range_max; i++){ 
             repel_lateral += (1/laser_scan_.ranges[i])*sin(i * M_PI / 180);
             repel_lateral += (1/laser_scan_.ranges[359-i])*sin(i * M_PI / 180);
             
             repel_drive += 1/laser_scan_.ranges[i]*cos(i * M_PI / 180);
             repel_drive += 1/laser_scan_.ranges[359-i]*cos(i * M_PI / 180);
     }
-  return State(repel_drive,repel_lateral);
+    
+  return State(repel_drive, repel_lateral);
 }
 
