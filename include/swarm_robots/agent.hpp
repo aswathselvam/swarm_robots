98210d74fe92dd7014f932a1c7bcc24ccbfc88e6
Kavyashree <kavya27.devadiga@gmail.com>
2021-12-12 19:45:36 -0500

    Task 4.1
    Added doxygen method nd variable documentation to agent class

--- include/swarm_robots/agent.hpp
+++ include/swarm_robots/agent.hpp
@@ -1,5 +1,5 @@
 /**
- * @file agent.h
+ * @file agent.hpp
  * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam
  * (aswath@umd.edu)
  * @brief The header file for agent control
@@ -28,22 +28,44 @@ using std::string;
 
 class Agent {
    public:  //  NOLINT
+    /**
+     * @brief Agent parameterized constructor,
+     * initialized agent with given id
+     * @param id : id of the agent - assigned to initialized
+     * Agent class object
+     */
     explicit Agent(string agent_id);
+
+    /**
+     * @brief Invoke agent path planeer
+     */
     void PlanPath();
+
+    /**
+     * @brief Invokes IK method from inverse kinematics class
+     */
     void PerformInverseKinematics();
+
+    /**
+     * @brief Invokes FK method from forward kinematics class
+     */
     void PerformForwardKinematics();
+
+    /**
+     * @brief Sets all velocities to zero
+     */
     void Stop();
 
-   protected:  //  NOLINT
-    State position_;
-    State velocity_;
-    double heading_angle_;
-    State goal_pos_;
-    PathPlanner* path_planner_;
-    ForwardKinematics* forward_kinematics_;
-    InverseKinematics* inverse_kinematics_;
+   protected:                                //  NOLINT
+    State position_;                         ///< Variable to store coordinates of agent
+    State velocity_;                         ///< Variable to store x,y,z velocities of agent
+    double heading_angle_;                   ///< Variable to calculate agngle for IK
+    State goal_pos_;                         ///< Variable to store x,y,z of destination
+    PathPlanner* path_planner_;              ///< PathPlanner object for agent node
+    ForwardKinematics* forward_kinematics_;  ///< ForwardKinematics object for agent node
+    InverseKinematics* inverse_kinematics_;  ///< InverseKinematics object for agent node
 
-   private:     //  NOLINT
-    string agent_id_;
+   private:            //  NOLINT
+    string agent_id_;  ///< ID of agent to refer it's namespace
 };
 #endif  // INCLUDE_SWARM_ROBOTS_AGENT_HPP_
