9bc5cf02bf9d3981a6ff4cb8e38df231669ba847
Kavyashree <kavya27.devadiga@gmail.com>
2021-12-13 02:34:24 -0500

    Task 3.1
    Add test methods

--- test/testswarm.cpp
+++ test/testswarm.cpp
@@ -12,24 +12,53 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <ros/service_client.h>
-#include "std_msgs/String.h"
 
-#include "swarm_robots/agent.h"
-#include "swarm_robots/arena.h"
+#include "../include/swarm_robots/agent.hpp"
+#include "../include/swarm_robots/arena.hpp"
+#include "../include/swarm_robots/forward_kinematics.hpp"
+#include "std_msgs/String.h"
+#include "swarm_robots/agent_node.hpp"
 
 /**
  * @brief Test if Initialization is successful
  */
 TEST(TestStub1, TestArentsInit) {
-  ros::NodeHandle nh;
-  Arena arena;
-  EXPECT_TRUE(arena.InitializeAgents());
+    vector<AgentNode*> agent_nodes;
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        AgentNode* agent_node = new AgentNode(std::to_string(i));
+        agent_nodes.push_back(agent_node);
+    }
+    bool flag = true;
+    for (int i = 0; i < n; i++) {
+        if (agent_nodes[i] == NULL) flag = false;
+    }
+    EXPECT_TRUE(flag);
+}
+
+/**
+ * @brief Tests Agent class initialization
+ */
+TEST(TestStub2, TestAgentClassInit) {
+    Agent agent_("2");
+    EXPECT_EQ(agent_.heading_angle_, 180);
 }
 
 /**
  * @brief Tests total agents spawned
  */
-TEST(TestStub2, TestSwarmSize) {
-  Arena arena;
-  EXPECT_EQ(arena.GetSwarmSize(), 4);
+TEST(TestStub3, TestSwarmSize) {
+    Arena arena;
+    EXPECT_EQ(arena.GetSwarmSize(), 20);
+}
+
+/**
+ * @brief Test FK Move forward method
+ *
+ */
+TEST(TestStub7, TestMoveForward) {
+    geometry_msgs::Twist robot_vel;  // Robot twist message object
+    ForwardKinematics FK;
+    FK.MoveForward(&robot_vel);
+    EXPECT_GT(robot_vel.linear.x, 0);
 }
